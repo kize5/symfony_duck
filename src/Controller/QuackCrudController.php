3d5a490b43e42f30cd5d5218f5b82d99bc27a4fc
kize <bochu.nicolas@hotmail.fr>
2023-03-20 16:26:05 +0100

    first commit

---
+++ src/Controller/QuackCrudController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Quack;
+use App\Form\QuackType;
+use App\Repository\QuackRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/quack/crud')]
+class QuackCrudController extends AbstractController
+{
+    #[Route('/', name: 'app_quack_crud_index', methods: ['GET'])]
+    public function index(QuackRepository $quackRepository): Response
+    {
+        return $this->render('quack_crud/index.html.twig', [
+            'quacks' => $quackRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_quack_crud_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, QuackRepository $quackRepository): Response
+    {
+        $quack = new Quack();
+        $form = $this->createForm(QuackType::class, $quack);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $quackRepository->save($quack, true);
+
+            return $this->redirectToRoute('app_quack_crud_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('quack_crud/new.html.twig', [
+            'quack' => $quack,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_quack_crud_show', methods: ['GET'])]
+    public function show(Quack $quack): Response
+    {
+        return $this->render('quack_crud/show.html.twig', [
+            'quack' => $quack,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_quack_crud_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Quack $quack, QuackRepository $quackRepository): Response
+    {
+        $form = $this->createForm(QuackType::class, $quack);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $quackRepository->save($quack, true);
+
+            return $this->redirectToRoute('app_quack_crud_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('quack_crud/edit.html.twig', [
+            'quack' => $quack,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_quack_crud_delete', methods: ['POST'])]
+    public function delete(Request $request, Quack $quack, QuackRepository $quackRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$quack->getId(), $request->request->get('_token'))) {
+            $quackRepository->remove($quack, true);
+        }
+
+        return $this->redirectToRoute('app_quack_crud_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Quack.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuackRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuackRepository::class)]
+class Quack
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(string $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
